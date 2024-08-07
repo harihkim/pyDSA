df4ff777a9ad2306ca15be113a2d185d0e8352fc
Harikrishnan <hkupim@gmail.com>
2024-08-07 16:43:44 +0530

    changed the order of functions

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,3 +1,14 @@
+def selectionSort(arr: list[int | float]):
+    size = len(arr)
+    for i in range(size - 1):
+        maxIndex = 0
+        for j in range(size - i):
+            if arr[j] > arr[maxIndex]:
+                maxIndex = j
+        arr[size - 1 - i], arr[maxIndex] = arr[maxIndex], arr[size - i - 1]
+    return arr
+
+
 def selectionSort1(arr: list[int | float]):
     size = len(arr)
     for i in range(size - 1):
@@ -9,15 +20,4 @@ def selectionSort1(arr: list[int | float]):
     return arr
 
 
-def selectionSort0(arr: list[int | float]):
-    size = len(arr)
-    for i in range(size - 1):
-        maxIndex = 0
-        for j in range(size - i):
-            if arr[j] > arr[maxIndex]:
-                maxIndex = j
-        arr[size - 1 - i], arr[maxIndex] = arr[maxIndex], arr[size - i - 1]
-    return arr
-
-
-print(selectionSort0([2, 1]))
+print(selectionSort([2, 1]))
