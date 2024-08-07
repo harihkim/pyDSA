d3da378ee11e0f7f10a6645221b380ee9cb56bad
Harikrishnan <hkupim@gmail.com>
2024-08-07 16:28:09 +0530

    fixed a extra loop iteration in bubbleSort

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,19 +1,20 @@
-
-#inplace
+# inplace
 def bubble_sort(arr: list):
     size = len(arr)
 
-    for i in range(size):
+    for i in range(size - 1):
         swaped = False
-        for j in range(size-i-1):
-            if(arr[j]>arr[j+1]):
-                arr[j],arr[j+1] = arr[j+1], arr[j]
-                swaped=True
-        if(not swaped):
-           break
+        for j in range(size - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swaped = True
+        if not swaped:
+            break
         else:
             swaped = False
 
-myArr = [8,5,7,3,6]
-bubble_sort(myArr)
-print(myArr)
+
+if __name__ == "__main__":
+    myArr = [4, 3, 2, 1]
+    bubble_sort(myArr)
+    print(myArr)
