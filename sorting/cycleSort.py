c7a7ef0ce7d722e7b51df672a7abb0cf86a5ab94
Harikrishnan <hkupim@gmail.com>
2024-08-09 15:21:27 +0530

    added new implementations of cycle sort

--- sorting/cycleSort.py
+++ sorting/cycleSort.py
@@ -1,10 +1,33 @@
 def cycleSort(arr: list[int]):
     i = 0
+    while i < len(arr):
+        if arr[i] != i + 1:
+            temp = arr[i]
+            arr[i] = arr[temp - 1]
+            arr[temp - 1] = temp
+        else:
+            i += 1
+
+
+def cycleSort1(arr: list[int]):
+    i = 0
     for i in range(len(arr)):
         while arr[i] != i + 1:
             temp = arr[i]
             arr[i] = arr[temp - 1]
             arr[temp - 1] = temp
+
+
+def cycleSort2(arr: list[int]):
+    i = 0
+    while i < len(arr):
+        correctIndex = arr[i] - 1
+        if arr[i] != arr[correctIndex]:
+            temp = arr[correctIndex]
+            arr[correctIndex] = arr[i]
+            arr[i] = temp
+        else:
+            i += 1
 
 
 if __name__ == "__main__":
